29ee8eb1bc88f5fd21e4a1ea60254ebf04f5c2c3
“muhhjam” <muhhjam@gmail.com>
2024-02-25 15:03:14 +0700

    update

--- src/components/content/RegistView.vue
+++ src/components/content/RegistView.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="py-[40px] bg-[#2d2b46]">
-      <div class="max-w-[1080px] mx-auto flex justify-between">
+      <div class="max-w-[1080px] mx-auto flex flex-col md:flex-row justify-center gap-6 md:justify-between items-center">
         <div>
-        <h2 class="text-2xl font-bold text-white">Pendaftaran Lomba PSS</h2>
-        <h4 class="font-medium uppercase text-white">Udah dibuka nih!</h4>
+        <h2 class="text-2xl font-bold text-white text-center md:text-start">Pendaftaran Lomba PSS</h2>
+        <h4 class="font-medium uppercase text-white text-center md:text-start">Udah dibuka nih!</h4>
       </div>
       <div>
         <router-link to="/daftar" class="border-2 border-white text-white text-xl font-semibold px-4 py-2">Daftar ah</router-link>
