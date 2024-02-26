6752960cd2210f38eedd0bced21451e42934dd08
“muhhjam” <muhhjam@gmail.com>
2024-02-26 23:10:21 +0700

    update

--- src/components/content/RegistView.vue
+++ src/components/content/RegistView.vue
@@ -6,34 +6,14 @@
         <h4 class="font-medium uppercase text-white text-center md:text-start">Udah dibuka nih!</h4>
       </div>
       <div>
-        <router-link to="/daftar" class="border-2 border-white text-white text-xl font-semibold px-4 py-2 hover:opacity-[0.8]">Daftar ah</router-link>
+        <a href="/daftar" class="border-2 border-white text-white text-xl font-semibold px-4 py-2 hover:opacity-[0.8]">Daftar ah</a>
     </div>
     </div>
     </div>
   </template>
   
   <script>
-  export default {
-    data() {
-      return {
-        daftarLomba: [
-          { nama: "Jaipong Tunggal", deskripsi: "SD, SMP, dan SMA", gambar: require('@/assets/images/image1.jpg') },
-          { nama: "Jaipong Rampak", deskripsi: "SD, SMP, dan SMA", gambar: require('@/assets/images/image1.jpg') },
-          { nama: "Pupuh", deskripsi: "SMP dan SMA", gambar: require('@/assets/images/image1.jpg') },
-          { nama: "Sajak", deskripsi: "SD dan SMP", gambar: require('@/assets/images/image1.jpg') },
-          { nama: "Seni Ibing Pencak Silat", deskripsi: "SMP dan SMA", gambar: require('@/assets/images/image1.jpg') }
-        ]
-      };
-    },
-    methods: {
-      lihatDetail(lomba) {
-        console.log("Melihat detail lomba:", lomba);
-      }
-    }
-  };
   </script>
   
-  <style scoped>
-  /* Styling sesuai kebutuhan Anda */
-  </style>
+  
   
