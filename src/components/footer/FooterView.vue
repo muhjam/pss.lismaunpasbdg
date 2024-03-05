6d464b0f2f94bf1acbb1acf458743197c0c855a9
“muhhjam” <muhhjam@gmail.com>
2024-03-05 23:00:23 +0700

    update

--- src/components/footer/FooterView.vue
+++ src/components/footer/FooterView.vue
@@ -13,7 +13,7 @@
         <div>
           <p class="text-lg mb-2"><strong>Contact Us:</strong></p>
           <ul>
-            <li><span class="text-sm">Whatsapp: </span> <a href="http://wa.me/6285156632360?text=Aku%20Anya%20panitia%20PSS,%20ada%20yang%20bisa%20saya%20bantu?" target="_blank" class="text-sm hover:text-gray-400">+62 851-5663-2360 </a></li>
+            <li><span class="text-sm">Whatsapp: </span> <a href="http://wa.me/6285156632360?text=Hallo%20kak,%20ada%20yang%20saya%20bisa%20bantu?" target="_blank" class="text-sm hover:text-gray-400">+62 851-5663-2360 (Anya)</a></li>
             <li><span class="text-sm">Email: </span> <a href="mailto:psslismaunpas@gmail.com" target="_blank" class="text-sm hover:text-gray-400">psslismaunpas@gmail.com</a></li>
             <li><span class="text-sm">Address: </span> <a href="https://maps.app.goo.gl/5zWyjKzD2Kq7cVaw8" class="text-sm hover:text-gray-400">Jl. Tamansari No. 6-8 Kota Bandung, Jawa Barat Indonesia</a></li>
           </ul>
